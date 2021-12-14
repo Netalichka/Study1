0a225ffc5dde4b9f3ae96f1ea6dac1a3ed1815ea
Netalichka <netalichka2@gmail.com>
2021-12-14 10:20:07 +0300

    change1

--- Study1/Program.cs
+++ Study1/Program.cs
@@ -6,6 +6,23 @@
  Console.WriteLine("Привет Мир");
  Console.ReadLine();
 
+
 Console.WriteLine("Анкета");
+Console.WriteLine("Введите Ваше Имя:");
 
 string name =  Console.ReadLine();
+Console.WriteLine("Введите Вашу Фамилию:");
+
+string Lastname = Console.ReadLine();
+
+Console.WriteLine("Введите Ваш Рост:");
+
+string growth = Console.ReadLine();
+
+Console.WriteLine("Введите Ваше Вес:");
+
+string weight = Console.ReadLine();
+
+Console.WriteLine("ФИО " + name + Lastname + "Рост = " + growth + "Вес = " + weight);
+Console.ReadLine();
+
